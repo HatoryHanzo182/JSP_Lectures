afb836946ad03c12db55423ebdd9322b09aedf8b
HatoryHanzo <scrinnpowerapp@gmail.com>
2023-10-10 21:02:16 +0300

    Data about the browser (User-Agent) is transmitted in the request headers Task: create a filter that analyzes this data and creates "browser" attribute that stores information from this header. ** add the browserType attribute (desktopWin, mobile, desktopIos, ...)     which follows from the previous analysis.     https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent Register the filter, display parameters from it on the /filters page

--- src/main/java/step/learning/filters/BrowserTypeFilter.java
+++ src/main/java/step/learning/filters/BrowserTypeFilter.java
@@ -1,2 +1,36 @@
-package step.learning.filters;public class BrowserTypeFilter {
+package step.learning.filters;
+
+import com.google.inject.Singleton;
+import eu.bitwalker.useragentutils.UserAgent;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Singleton
+public class BrowserTypeFilter implements Filter
+{
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException
+    {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String userAgentString = request.getHeader("User-Agent");
+        UserAgent userAgent = UserAgent.parseUserAgentString(userAgentString);
+        String browserType = userAgent.getBrowser().getName();
+
+        request.setAttribute("browserType", browserType);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+
+    }
+
+    @Override
+    public void destroy()
+    {
+
+    }
 }
